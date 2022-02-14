3fdb6432972cf3f7c26148ae84af29d09c63681f
satyamtomar <satyamtomar@users.noreply.github.com>
2022-02-14 13:25:28 +0530

    update

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,60 +1,53 @@
 const User = require("../models/User");
-const {  validationResult,body } = require("express-validator");
+const { validationResult, body } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const jwtsecret = "satyamtomar";
 
 //Route1 Creating a user using: POST "/api/auth/createuser".Doesn't require Login
-module.exports={
-    createuser:
-     
-    
-     async (req, res) => {
+module.exports = {
+  createuser: async (req, res) => {
+    let success = false;
+    const errors = validationResult(req);
+    //if there are errors, returns bad request
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ success, errors: errors.array() });
+    }
+    //checks whether the email has already been created
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({
+          success,
+          error: "Sorry a user with this email already exists",
+        });
+      }
+      const salt = await bcrypt.genSalt(10);
 
-        let success = false;
-        const errors = validationResult(req);
-        //if there are errors, returns bad request
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ success, errors: errors.array() });
-        }
-        //checks whether the email has already been created
-        try {
-          let user = await User.findOne({ email: req.body.email });
-          if (user) {
-            return res
-              .status(400)
-              .json({
-                success,
-                error: "Sorry a user with this email already exists",
-              });
-          }
-          const salt = await bcrypt.genSalt(10);
-    
-          const secpass = await bcrypt.hash(req.body.password, salt);
-          user = await User.create({
-            name: req.body.name,
-            password: secpass,
-            email: req.body.email,
-          });
-          //   .then(user => res.json(user))
-          //   .catch(err=>{console.log(err)
-          // res.json({error:'Please enter a unique value for email'})
-          const data = {
-            user: {
-              id: user.id,
-            },
-          };
-          success = true;
-          const authtoken = jwt.sign(data, jwtsecret);
-          res.json({ success, authtoken });
-        } catch (error) {
-          console.error(error.message);
-          res.status(500).send("Internal server error");
-        }
-      } ,
+      const secpass = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
+        name: req.body.name,
+        password: secpass,
+        email: req.body.email,
+      });
+      //   .then(user => res.json(user))
+      //   .catch(err=>{console.log(err)
+      // res.json({error:'Please enter a unique value for email'})
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      success = true;
+      const authtoken = jwt.sign(data, jwtsecret);
+      res.json({ success, authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error");
+    }
+  },
 
-login:    async (req, res) => {
-  
+  login: async (req, res) => {
     let success = false;
     const errors = validationResult(req);
     //if there are errors, returns bad request
@@ -87,9 +80,9 @@ login:    async (req, res) => {
       console.error(error.message);
       res.status(500).send("Internal server error");
     }
-  }   ,
+  },
 
-  getuser:async (req, res) => {
+  getuser: async (req, res) => {
     try {
       const userId = req.user.id;
       const user = await User.findById(userId).select("-password");
@@ -98,6 +91,5 @@ login:    async (req, res) => {
       console.error(error.message);
       res.status(500).send("Internal server error");
     }
-  }
-
-}
+  },
+};
--- server/routes/list.js
+++ server/routes/list.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const fetchuser = require("../middleware/fetchuser");
-const listss=require("../controllers/list")
-const evalid=require("../middleware/expressvalid")
-router.route('/fetchlist').get(fetchuser,evalid.validate("vfetchlist"),listss.fetchlist);
+const listss = require("../controllers/list");
+const evalid = require("../middleware/expressvalid");
+router
+  .route("/fetchlist")
+  .get(fetchuser, evalid.validate("vfetchlist"), listss.fetchlist);
 
-router.route("/addlist").post(fetchuser,evalid.validate("vaddlist"),listss.addlist)
+router
+  .route("/addlist")
+  .post(fetchuser, evalid.validate("vaddlist"), listss.addlist);
 
-router.route('/updatelist/:id').put(fetchuser,listss.updatelist);
+router.route("/updatelist/:id").put(fetchuser, listss.updatelist);
 
-router.route('/deletelist/:id').delete(fetchuser,listss.deletelist);
-
+router.route("/deletelist/:id").delete(fetchuser, listss.deletelist);
 
 module.exports = router;
