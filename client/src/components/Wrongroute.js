7db7178f44f3039496219e808dc2995539a70c49
satyamtomar <satyamtomar@users.noreply.github.com>
2022-02-14 09:58:23 +0530

    update

--- client/src/components/Wrongroute.js
+++ client/src/components/Wrongroute.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Wrongroute = () => {
   return (
-    <div><h1>Error 404</h1>
-    <p>Page Not Found</p> </div>
+    <div  className="flex flex-row justify-center">
+    <h1 className="text-4xl">Error 404</h1>
+    <p className="text-2xl">Page Not Found</p> </div>
   )
 }
 
